3de3b322d5d5528490c11a8eae64e94c9820a919
Oscar Eriksson <oscar@thenetcircle.com>
2022-01-07 03:56:54 +0800

    formatted docs

--- list.js
+++ list.js
@@ -5,8 +5,8 @@ export async function main(ns) {
 	const origin = []
 	const all_servers = []
 
-	// remove all bought servers that are scanable from home, they
-	// have no money anyway
+	// remove all bought servers that are scanable from home, they have no
+	// money anyway
 	for (let i = 0; i < origin_unfiltered.length; i++) {
 		if (purchased.includes(origin_unfiltered[i])) {
 			 continue
@@ -14,9 +14,8 @@ export async function main(ns) {
 		origin.push(origin_unfiltered[i])
 	}
 
-	// recursively scan every server that is scanable from the
-	// 'home' server; all servers in the game will be listed in
-	// 'all_servers'
+	// recursively scan every server that is scanable from the 'home' server;
+	// all servers in the game will be listed in 'all_servers'
 	for (let i = 0; i < origin.length; i++) {
 		recursively_scan_these_servers(ns, origin[i], all_servers)
 	}
@@ -37,40 +36,43 @@ export async function main(ns) {
 		})
 	}
 
-	// sort each server in the game not by their distance, but
-	// by their hack level, so we can print what next to hack
+	// sort each server in the game not by their distance, but by their hack
+	// level, so we can print what next to hack
 	const sorted = server_and_hack_lvl.sort(
 		(s1, s2) => s1.level - s2.level,
 	)
 
-	// now print each server that we have NOT yet root access to,
-	// but we DO HAVE a hack level high enough for to nuke
+	// now print each server that we have NOT yet root access to, but we DO
+	// HAVE a hack level high enough for to nuke
 	for (let i = 0; i < sorted.length; i++) {
 		const name = sorted[i].name
 		const level = sorted[i].level
 
 		if (!ns.hasRootAccess(name)) {
+			const server_max_money = ns.getServerMaxMoney(name)
+
 			ns.tprint(
 				name.padEnd(20),
 				level.toFixed(0).toString().padEnd(12),
-				ns.getServerMaxMoney(name).toFixed(0).toString().padEnd(15)
+				server_max_money.toFixed(0).toString().padEnd(15)
 			)
 		}
 	}
 }
 
 export function recursively_scan_these_servers(ns, s, all_servers) {
+    // run scan() on 'this' arbitrary server (since this method is recursive)
 	const new_servers = ns.scan(s)
 
 	for (let i = 0; i < new_servers.length; i++) {
-		// avoid an infinite loop, only add servers to scan that we
-		// haven't scanned before
+		// avoid an infinite loop, only add servers to scan that we haven't
+		// scanned before
 		if (all_servers.includes(new_servers[i])) {
 			continue
 		}
 
-		// add the unique servers to the 'all_servers' list, which
-		// we will be accessing in the main() function afterwards
+		// add the unique servers to the 'all_servers' list, which we will be
+		// accessing in the main() function afterwards
 		all_servers.push(new_servers[i])
 
 		// now scan every server that is scannable from this server,
